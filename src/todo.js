e6b97243362c3149830bac69623317b3d19e274d
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2024-02-18 09:46:00 -0500

    Edit functions to create and add todo items

--- src/todo.js
+++ src/todo.js
@@ -1,8 +1,31 @@
+import { getValues,} from "./dialog";
+import { checkPriority } from "./priority";
+import { projects,} from "./create-project";
+import makeItemCard from "./render";
+
 export default class ToDo {
     constructor(title, description, dueDate, priority) {
         this.title = title;
         this.description = description;
-        this.dueDate = dueDate;
+        this.due = dueDate;
         this.priority = priority;
     }
-}
+};
+
+function createTodo () {
+    getValues();
+    const newItem = new ToDo(getValues().todoTitle, getValues().todoDsc, getValues().todoDue, checkPriority());
+    projects[0].push(newItem);
+};
+
+function renderTodo () {
+    for (let i = 0; i < projects.length; i++) {
+        for (let project in projects[i]) {
+            let item = projects[i][project];
+            makeItemCard(item);
+        }
+    }
+};
+
+
+export {createTodo, renderTodo,}
